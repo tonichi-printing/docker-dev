e4ffb68eff10400d83580ebf74cdd62a0604a1e8
Lav Shinde <13927503+dragoste17@users.noreply.github.com>
2020-11-06 15:27:33 +0900

    Update to use separate PHP and Nginx Containers

--- build.sh
+++ build.sh
@@ -14,9 +14,9 @@ ssh -i /Users/mb21706/Work/dev/keys/tonichiaws.pem -t ec2-user@ec2-54-248-34-26.
   cd docker-dev
   git pull
 
-  docker build -t php-nginx-server:latest-dev -f php-nginx-server/Dockerfile ./php-nginx-server
-  docker tag php-nginx-server:latest-dev 477120415291.dkr.ecr.ap-northeast-1.amazonaws.com/dev:php-nginx-server
-  docker push 477120415291.dkr.ecr.ap-northeast-1.amazonaws.com/dev:php-nginx-server
+  docker build -t php:latest-dev -f php/Dockerfile ./php
+  docker tag php:latest-dev 477120415291.dkr.ecr.ap-northeast-1.amazonaws.com/dev:php
+  docker push 477120415291.dkr.ecr.ap-northeast-1.amazonaws.com/dev:php
 
   docker build -t queue-scheduler:latest-dev -f queue-scheduler/Dockerfile ./queue-scheduler
   docker tag queue-scheduler:latest-dev 477120415291.dkr.ecr.ap-northeast-1.amazonaws.com/dev:queue-scheduler
--- php-nginx-server/start-fpm.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/with-contenv sh
-set -e;
-
-# Start PHP-FPM
-/usr/sbin/php-fpm${PHP_MAJOR_VERSION} -R --nodaemonize
