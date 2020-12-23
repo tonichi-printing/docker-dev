b7dbf836a509c3bdb65ac0f91d94c4e768142b4f
Lav Shinde <13927503+dragoste17@users.noreply.github.com>
2020-12-24 08:15:12 +0900

    Add redis for Meishi

--- build.sh
+++ build.sh
@@ -6,19 +6,12 @@ if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
   exit 0
 fi
 
-ssh -i /Users/mb21706/Work/dev/keys/tonichiaws.pem -t ec2-user@ec2-54-248-34-26.ap-northeast-1.compute.amazonaws.com << EOF
-  aws ecr get-login-password --region ap-northeast-1 | docker login --username AWS --password-stdin 477120415291.dkr.ecr.ap-northeast-1.amazonaws.com/dev
+aws ecr get-login-password --region ap-northeast-1 | docker login --username AWS --password-stdin 477120415291.dkr.ecr.ap-northeast-1.amazonaws.com/dev
 
-  sudo service docker start
+docker build -t php:latest-dev -f php/Dockerfile ./php
+docker tag php:latest-dev 477120415291.dkr.ecr.ap-northeast-1.amazonaws.com/dev:php
+docker push 477120415291.dkr.ecr.ap-northeast-1.amazonaws.com/dev:php
 
-  cd docker-dev
-  git pull
-
-  docker build -t php:latest-dev -f php/Dockerfile ./php
-  docker tag php:latest-dev 477120415291.dkr.ecr.ap-northeast-1.amazonaws.com/dev:php
-  docker push 477120415291.dkr.ecr.ap-northeast-1.amazonaws.com/dev:php
-
-  docker build -t queue-scheduler:latest-dev -f queue-scheduler/Dockerfile ./queue-scheduler
-  docker tag queue-scheduler:latest-dev 477120415291.dkr.ecr.ap-northeast-1.amazonaws.com/dev:queue-scheduler
-  docker push 477120415291.dkr.ecr.ap-northeast-1.amazonaws.com/dev:queue-scheduler
-EOF
+docker build -t queue-scheduler:latest-dev -f queue-scheduler/Dockerfile ./queue-scheduler
+docker tag queue-scheduler:latest-dev 477120415291.dkr.ecr.ap-northeast-1.amazonaws.com/dev:queue-scheduler
+docker push 477120415291.dkr.ecr.ap-northeast-1.amazonaws.com/dev:queue-scheduler
