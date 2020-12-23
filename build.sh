4bc60724eefe2e91163122b1935da5c5542e1a91
Lav Shinde <13927503+dragoste17@users.noreply.github.com>
2020-12-24 08:18:05 +0900

    Keep Single Image for All PHP based containers

--- build.sh
+++ build.sh
@@ -11,7 +11,3 @@ aws ecr get-login-password --region ap-northeast-1 | docker login --username AWS
 docker build -t php:latest-dev -f php/Dockerfile ./php
 docker tag php:latest-dev 477120415291.dkr.ecr.ap-northeast-1.amazonaws.com/dev:php
 docker push 477120415291.dkr.ecr.ap-northeast-1.amazonaws.com/dev:php
-
-docker build -t queue-scheduler:latest-dev -f queue-scheduler/Dockerfile ./queue-scheduler
-docker tag queue-scheduler:latest-dev 477120415291.dkr.ecr.ap-northeast-1.amazonaws.com/dev:queue-scheduler
-docker push 477120415291.dkr.ecr.ap-northeast-1.amazonaws.com/dev:queue-scheduler
